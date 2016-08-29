e7b52eec5cab10de75adfc100ae0519ebac913dc
santyu <santyu.tyu.tyu.tyu@gmail.com>
2016-08-29 15:12:31 +0000

    key名変更

--- app/controllers/api/v1/twitters_controller.rb
+++ app/controllers/api/v1/twitters_controller.rb
@@ -18,7 +18,7 @@ module Api
               created_at: tweet.created_at,
               account:    tweet.user.screen_name,
               contents:   tweet.full_text,
-              thumbnail:  tweet.media.first.media_url.to_s,
+              thumb_url:  tweet.media.first.media_url.to_s,
               video_url:  tweet.media.first.expanded_url.to_s,
             }
         end
