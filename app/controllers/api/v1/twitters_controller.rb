ff42808c8e2bd7d8989014050ecf0f98609a2943
santyu <santyu.tyu.tyu.tyu@gmail.com>
2016-11-28 15:17:54 +0000

    投稿に含まれる位置情報を取得

--- app/controllers/api/v1/twitters_controller.rb
+++ app/controllers/api/v1/twitters_controller.rb
@@ -11,21 +11,27 @@ module Api
         )
 
         # 公式RTなしの最新のリンク付きの投稿を100件(GET最大値)取得する
+        # geocode 例) 35.681382,139.766084,20km "緯度,経度,距離" のフォーマット
         options = { geocode: params[:geocode], result_type: 'recent', count: 100, exclude: 'retweets', filter: 'links' }
-        tweets  = client.search(params[:query], options).take(100)
+        tweets = client.search(params[:query], options).take(100)
         # 動画のある投稿に絞る
         tweets.keep_if { |tweet| tweet.media.first.present? && tweet.media.first.expanded_url.to_s.include?('video') }
+
+        # 位置情報を含む投稿に絞る
+        tweets.keep_if { |tweet| tweet.user.status.place.bounding_box.coordinates.present? }
 
         data = []
         tweets.each do |tweet|
           data << {
-            tweet_id:   tweet.id,
-            created_at: tweet.created_at.to_s,
-            account:    tweet.user.screen_name,
-            contents:   tweet.full_text,
-            thumb_url:  tweet.media.first.media_url.to_s,
-            video_url:  tweet.media.first.expanded_url.to_s,
-            player_url: tweet.media.first.video_info.variants.first.url.to_s
+            tweet_id:      tweet.id,
+            created_at:    tweet.created_at.to_s,
+            account:       tweet.user.screen_name,
+            contents:      tweet.full_text,
+            thumb_url:     tweet.media.first.media_url.to_s,
+            video_url:     tweet.media.first.expanded_url.to_s,
+            player_url:    tweet.media.first.video_info.variants.first.url.to_s,
+            location_name: tweet.user.status.place.full_name,
+            location_code: tweet.user.status.place.bounding_box.coordinates
           }
         end
 
